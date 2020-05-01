75ce92e0831f76a50790a32f4cdaa75b711be124
Eddyszh <eddyszh@gmail.com>
2020-05-01 00:36:09 +0000

    Fix tasks 3,5

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    sum = 0
     for i in sys.argv[1:]:
-        if i.isdigit() == False:
-            sys.exit("All the arguments must be integers")
-    num = [int(i) for i in sys.argv[1:]]
-    sum = sum(num)
+        sum += int(i)
     print("{:d}".format(sum))
