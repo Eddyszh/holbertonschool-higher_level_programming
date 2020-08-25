677c728cfebf4441e21a8b6f95ce87c259f23e86
eddyszh <eddyszh@gmail.com>
2020-08-24 22:46:36 -0500

    Fix misspelling issue

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     import requests
     from sys import argv
     r = requests.get(argv[1]).text
-    code = requests.status_codes
-    if code > 400:
+    code = r.status_codes
+    if code >= 400:
         print("Error code: {}".format(code))
     else:
         print(r)
