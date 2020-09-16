3e5740747630138ba2bfed5cb45bcbc5e4aaf376
eddyszh <eddyszh@gmail.com>
2020-09-15 23:35:01 -0500

    Fix length

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length < 4) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
   console.log(process.argv.splice(2, process.argv.length - 1).sort().reverse()[1]);
