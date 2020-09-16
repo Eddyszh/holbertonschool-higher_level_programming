779a09a682c9c68f24ab22ea9a708535e761f086
eddyszh <eddyszh@gmail.com>
2020-09-15 23:58:53 -0500

    Return last commit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,5 +2,5 @@
 if (process.argv.slice(2).length === 0 || process.argv.slice(2).length === 1) {
   console.log(0);
 } else {
-  console.log(process.argv.slice(2, process.argv.length - 1).sort().reverse()[1]);
+  console.log(process.argv.splice(2, process.argv.length - 1).sort().reverse()[1]);
 }
