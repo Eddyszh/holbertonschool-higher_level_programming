53865869618d3dc827048234e30ec8884f499f0a
eddyszh <eddyszh@gmail.com>
2020-09-21 23:01:10 -0500

    Fix append

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[3], function (err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     return;
   }
-  fs.appendFile(process.argv[4], data, function (err) {
+  fs.appendFileSync(process.argv[4], data, function (err) {
     if (err) {
 
     }
   });
 });
 
-fs.readFile(process.argv[2], function (err, data) {
+fs.readFile(process.argv[3], 'utf8', function (err, data) {
   if (err) {
     return;
   }
-  fs.appendFile(process.argv[4], data, function (err) {
+  fs.appendFileSync(process.argv[4], data, function (err) {
     if (err) {
 
     }
